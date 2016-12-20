c950de4025b578407896c25c78020fdf76ba17f7
Akihiro Kimura <aki.kimura@opt.ne.jp>
2016-12-20 16:58:33 +0900

    feat: Dynamic generate badge

--- src/get-svg.js
+++ src/get-svg.js
@@ -1,23 +1,21 @@
 const aws = require('aws-sdk');
-aws.config.update({ accessKeyId: '', secretAccessKey: '' });
 const s3 = new aws.S3({ apiVersion: '2006-03-01' });
 const http = require('https');
 const fs = require('fs');
 
-const url = `https://img.shields.io/badge/front--coverage-100%25-green.svg`
 const id = 'master';
 const imgType = 'svg';
-const tmpFileName = `./badge_${new Date().getTime()}`;
+const tmpFileName = `/tmp/badge_${new Date().getTime()}`;
 const tmpFile = fs.createWriteStream(tmpFileName);
 
-module.exports = function () {
+module.exports = function (url) {
   http.get(url, res => {
     res.pipe(tmpFile);
     res.on('end', () => {
       tmpFile.close();
       fs.readFile(tmpFileName, (err, data) => {
         const params = {
-          Key: `hgoe.svg`,
+          Key: `branch/master.svg`,
           Body: data,
           Bucket: 'zoi-public'
         };
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const aws = require('aws-sdk');
 const s3 = new aws.S3({apiVersion: '2006-03-01'});
 const http = require('https');
 const fs = require('fs');
+const put = require('./get-svg');
 
 // s3://zoi-coverage/
 //    -- report/master.json
@@ -16,79 +17,34 @@ exports.handler = (event, context) => {
   console.log('Received event:', JSON.stringify(event, null, 2));
   const bucket = event.Records[0].s3.bucket.name;
   const key = event.Records[0].s3.object.key;
-  s3.getObject({
-      Bucket: bucket,
-      Key: key
-    },
-    (err, data) => {
-      if (err) {
-        context.done('error', `error getting file${err}`);
-      } else {
-        console.log(`data:${JSON.stringify(data, null, ' ')}`);
-        try {
-          // Coverage Report Object
-          const cro = JSON.parse(data.Body);
-          if (cro.coverage) {
-            const coverage = Number(cro.coverage);
-            if (isNaN(coverage)) {
-              throw `property 'coverage' is not a number`;
-            }
-            const rounded = Math.round(coverage);
-            if (rounded < 0 || 100 < rounded) {
-              throw `property 'coverage' out of range:${rounded}`;
-            }
-            console.log(`coverage:${rounded}!!`); //FIXME
-
-        const bucket = 'zoi-public';
-
-        // generate badge
-        // const convert4shieldsIo = str => str.replace(/-/g, '--').replace(/_/g, '__');
-        // const subject = convert4shieldsIo(cro['subject']) || 'subject';
-        const url = `https://img.shields.io/badge/front--coverage-100%25-green.svg`
-        const id = 'master';
-        const imgType = 'svg';
-        const tmpFileName = `/tmp/badge_${new Date().getTime()}`;
-        const tmpFile = fs.createWriteStream(tmpFileName);
-        const req = http.get(url, res => {
-          res.pipe(tmpFile);
-          res.on('end', () => {
-            tmpFile.close();
-
-            fs.readFile(tmpFileName, (err, data) => {
-              if (err) context.done('error', err);
-
-              const params = {
-                Key: `${bucket}/branch/${id}.${imgType}`,
-                Body: data
-              };
-              const upload = (err, data) => {
-                if (err) context.done('error', `error put badge${err}`)
-              };
-              s3.upload(params, upload);
-            });
-          });
-        });
-
-
-        // // copy image
-
-        // const copySource = `${bucket}/image/front--coverage-${rounded}.${imgType}`;
-        // const params = {
-        //   CopySource: copySource,
-        //   Bucket: bucket,
-        //   Key: `branch/${id}.${imgType}`
-        // };
-        // s3.copyObject(params, (err, data) => {
-        //   if (err) { // an error occurred
-        //     console.error(err, err.stack);
-        //   } else { // successful response
-        //     console.log(data);
-        //   };
-        // });
-
-      } else {
-        throw `property 'coverage' not found`;
+  const getObject = (err, data) => {
+    if (err) context.done('error', `error getting file${err}`);
+    console.log(`data:${JSON.stringify(data, null, ' ')}`);
+    try {
+      // Coverage Report Object
+      const cro = JSON.parse(data.Body);
+      if (!cro.coverage) context.done('error', `property 'coverage' not found`);
+      const coverage = Number(cro.coverage);
+      if (isNaN(coverage)) {
+        throw `property 'coverage' is not a number`;
       }
+      const rounded = Math.round(coverage);
+      if (rounded < 0 || 100 < rounded) {
+        throw `property 'coverage' out of range:${rounded}`;
+      }
+      console.log(`coverage:${rounded}!!`); //FIXME
+
+      const bucket = 'zoi-public';
+
+      // generate badge
+      // const convert4shieldsIo = str => str.replace(/-/g, '--').replace(/_/g, '__');
+      // const subject = convert4shieldsIo(cro['subject']) || 'subject';
+      const url = `https://img.shields.io/badge/front--coverage-${coverage}%25-green.svg`
+
+      put(url);
+    } catch (e) {
+      context.done('error', `${e}`);
     }
-  );
+  };
+  s3.getObject({ Bucket: bucket, Key: key }, getObject);
 };
