b725a40fe45b9b085ae64ad72430dc166d0aacdc
sisisin <azsisisin@gmail.com>
2017-01-09 03:52:06 +0900

    fix: fail deploying when no dist/ and npm packages

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
 # Zone of integration
 
 
-# deploy
+# self deploy
 
 ```
-$ mkdir dist
 $ export AWS_ACCESS_KEY_ID=[access_key]
 $ export AWS_SECRET_ACCESS_KEY=[secret_key]
 $ bash ./deploy.sh
--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+mkdir -p dist
 cd src/
+npm install --production
 zip -r ../dist/lambda.zip ./*
 cd ..
 aws lambda update-function-code --function-name zoi-badge-batch --zip-file fileb://dist/lambda.zip
